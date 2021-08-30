c15ee2518175142fedeb18bb103ad63ee68dd86e
noidias <noidias1@gmail.com>
2021-08-30 21:23:13 +1200

    open retake section added and working

--- app.js
+++ app.js
@@ -43,25 +43,26 @@ function getExploredPlanetSummary(text) {
             exploredArray.push(match[3])
         }
     }
-    exploredCounts = occurrence(exploredArray)    
+    exploredCounts = occurrence(exploredArray)
     exploredSummary = `${dashes}Explored${dashes}`
     exploredSummary = buildReportSection(exploredCounts, exploredSummary, 'planet(s) Explored by', 'planet(s) Explored', true)
     return exploredSummary
 }
 
 function getCapturedPlanetSummary(text) {
-//The forces of niceguy took planet 8 in the 72,13 system from Justin_Bieber (6362).
-    capturePattern =/(\d+)[\s]SA[\s]T-(\d{1,4})\s+The forces of ([\w+\s*\w*]*) took planet (\d+) in the (\d+)[,:](\d+) system from ([\w+\s*\w*]*) .(\d+)../gm;
+    //The forces of niceguy took planet 8 in the 72,13 system from Justin_Bieber (6362).
+    capturePattern = /(\d+)[\s]SA[\s]T-(\d{1,4})\s+The forces of ([\w+\s*\w*]*) took planet (\d+) in the (\d+)[,:](\d+) system from ([\w+\s*\w*]*) .(\d+)../gm;
     matches = text.matchAll(capturePattern);
     capturedByPlayer = []
     capturedByFamily = []
     capturedfromEnemyPlayer = []
-    capturedPlanets = []
+    capturedPlanetsList = []
     if (matches !== null) {
         for (const match of matches) {
             capturedByPlayer.push(match[3])
             capturedfromEnemyPlayer.push(match[7])
             capturedByFamily.push(match[8])
+            capturedPlanetsList.push([match[1], match[2], `${match[5]},${match[6]}:${match[4]}`]) // extract linenumber, turn, planet
         }
     }
 
@@ -77,13 +78,13 @@ function getCapturedPlanetSummary(text) {
     capturedSummary = `${capturedSummary}${dashes}Captured by player${dashes}`
     capturedSummary = buildReportSection(playerCounts, capturedSummary, 'planet(s) captured by', 'planet(s) captured', true)
 
-    return capturedSummary
+    return [capturedSummary, capturedPlanetsList]
 }
 
 function getLostPlanetSummary(text) {
     //example
     //EA	T-96		After a brave fight our family member niceguy had to flee the planet planet 13 in the 100,10 system which was attacked by Justin_Bieber of family 6362.
-    defeatPattern =/(\d+)[\s]EA[\s]T-(\d{1,4})\s+After a brave fight our family member ([\w+\s*\w*]*) had to flee the planet planet (\d+) in the (\d+)[,:](\d+) system which was attacked by ([\w+\s*\w*]*) of family (\d+)+./gm;
+    defeatPattern = /(\d+)[\s]EA[\s]T-(\d{1,4})\s+After a brave fight our family member ([\w+\s*\w*]*) had to flee the planet planet (\d+) in the (\d+)[,:](\d+) system which was attacked by ([\w+\s*\w*]*) of family (\d+)+./gm;
     matches = text.matchAll(defeatPattern);
     capturedFromPlayer = []
     capturedByFamily = []
@@ -92,8 +93,7 @@ function getLostPlanetSummary(text) {
         for (const match of matches) {
             capturedFromPlayer.push(match[3])
             capturedByFamily.push(match[8])
-            // linenumber, turn, planet, family
-            lostPlanets.push([match[1],match[2],`${match[5]},${match[6]}:${match[4]}`,match[8]])
+            lostPlanets.push([match[1], match[2], `${match[5]},${match[6]}:${match[4]}`, match[8]]) //extract linenumber, turn, planet, family
         }
     }
 
@@ -105,20 +105,22 @@ function getLostPlanetSummary(text) {
     defeatSummary = `${defeatSummary}${dashes}Defeats by Player${dashes}`
     defeatSummary = buildReportSection(playerCounts, defeatSummary, 'planet(s) lost by', 'planet(s) lost', true)
 
-return [defeatSummary, lostPlanets]
+    return [defeatSummary, lostPlanets]
 }
 
-function getBlownUpAttacksSummary(text) {
+function getBlownUpCapturesSummary(text) {
     //example
     //SA	T-169		Zanharim attacked Mr_Hinx (6360) on planet 9 in the 68,10 system, and the heavy battle made the planet uninhabitable; an exploration ship will have to be sent there.
-    destroyedPattern =/(\d+)[\s]SA[\s]T-(\d{1,4})\s+([\w+\s*\w*]*) attacked ([\w+\s*\w*]*) .(\d+). on planet (\d+) in the (\d+)[,:](\d+) system, and the heavy battle made the planet uninhabitable; an exploration ship will have to be sent there./gm;
+    destroyedPattern = /(\d+)[\s]SA[\s]T-(\d{1,4})\s+([\w+\s*\w*]*) attacked ([\w+\s*\w*]*) .(\d+). on planet (\d+) in the (\d+)[,:](\d+) system, and the heavy battle made the planet uninhabitable; an exploration ship will have to be sent there./gm;
     matches = text.matchAll(destroyedPattern);
     destroyedByPlayer = []
     destroyedFamily = []
+    destroyedPlanetList = []
     if (matches !== null) {
         for (const match of matches) {
             destroyedByPlayer.push(match[3])
             destroyedFamily.push(match[5])
+            destroyedPlanetList.push([match[1], match[2], `${match[6]},${match[7]}:${match[5]}`]) //extract linenumber, turn, planet, family
         }
     }
 
@@ -129,17 +131,17 @@ function getBlownUpAttacksSummary(text) {
     playerCounts = occurrence(destroyedByPlayer)
     destroyedSummary = `${destroyedSummary}${dashes}Captures blown by player${dashes}`
     destroyedSummary = buildReportSection(playerCounts, destroyedSummary, 'planet(s) made uninhabitable by', 'planet(s) destroyed', true)
-    return destroyedSummary
+    return [destroyedSummary, destroyedPlanetList]
 }
 
-function getBlownUpDefetsSummary(text) {
+function getBlownUpDefeatsSummary(text) {
     //example
 
     //EA	T-129		An overwhelming force from Mr_Hinx, family 6360 attacked Who's planet 4 in the 93,83 system. The defenders for Who managed to set off a nuclear blast which made the planet uninhabitable.
     //eventTick+"An overwhelming force from "+playerNameRegex+", family (\\d+) attacked "+playerNameRegex+"'s"+planetRegex+". The defenders for "+playerNameRegex+" managed to set off a nuclear blast which made the planet uninhabitable.");
     //destroyedDefetsPattern =/(\d+)[\s]EA[\s]T-(\d{1,4})\s+An overwhelming force from ([\w+\s*\w*]*), family (\d+) attacked ([\w+\s*\w*]*)'s planet (\d+) in the (\d+)[,:](\d+) system. The defenders for ([\w+\s*\w*]*) managed to set off a nuclear blast which made the planet uninhabitable./gm;
-    destroyedDefetsPattern =/(\d+)[\s]..[\s]T-(\d{1,4})\s+An overwhelming force from ([\w+\s*\w*]*), family (\d+) attacked ([\w+\s*\w*]*).s planet (\d+) in the (\d+)[,:](\d+) system. The defenders for ([\w+\s*\w*]*).*/gm;
-    
+    destroyedDefetsPattern = /(\d+)[\s]..[\s]T-(\d{1,4})\s+An overwhelming force from ([\w+\s*\w*]*), family (\d+) attacked ([\w+\s*\w*]*).s planet (\d+) in the (\d+)[,:](\d+) system. The defenders for ([\w+\s*\w*]*).*/gm;
+
     matches = text.matchAll(destroyedDefetsPattern);
     destroyedEAByPlayer = []
     destroyedEAFamily = []
@@ -160,13 +162,31 @@ function getBlownUpDefetsSummary(text) {
     return destroyedEASummary
 }
 
-function findOpenRetakes(lostPlanets) {
+function findOpenRetakes(lostPlanets, capturedPlanets, blownUpCapturesPlanets) {
     openRetakeList = `${dashes}OPEN RETAKES${dashes}`
+    openRetakes = []
     //97,82:3 (#6360, lost Tick 171)
+    match = false
     for (let i = 0; i < lostPlanets.length; i++) {
-
-        openRetakeList = `${openRetakeList}${lostPlanets[i][2]} (${lostPlanets[i][3]}), lost Tick ${lostPlanets[i][1]}<br>`
-      }  
+        for (let j = 0; j < capturedPlanets.length; j++) {
+            if ((lostPlanets[i][2] === capturedPlanets[j][2]) && (capturedPlanets[j][0] < lostPlanets[i][0])) {
+                match = true;
+                break;
+            }
+        }
+        for (let k = 0; k < blownUpCapturesPlanets.length; k++) {
+            if ((lostPlanets[i][2] === blownUpCapturesPlanets[k][2]) && (blownUpCapturesPlanets[k][0] < lostPlanets[i][0])) {
+                match = true;
+                break;
+            }
+        }
+        if (!match) {
+            openRetakes.push(lostPlanets[i])
+            openRetakeList = `${openRetakeList}${lostPlanets[i][2]} (${lostPlanets[i][3]}), lost Tick ${lostPlanets[i][1]}<br>`
+        }
+        match = false
+    }
+    openRetakeList = `${openRetakeList}${dashes}Total Missing ${openRetakes.length}${dashes}`
     return openRetakeList
 
 }
@@ -179,7 +199,7 @@ function buildReportSection(array, sectionSummary, textLine1, textLine2, include
             total = total + array[item].length
         }
         if (includeSummary) {
-        sectionSummary = `${sectionSummary}${dashes}${total} ${textLine2}${dashes}`
+            sectionSummary = `${sectionSummary}${dashes}${total} ${textLine2}${dashes}`
         }
     }
     return sectionSummary
